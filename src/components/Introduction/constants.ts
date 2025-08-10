68fb633004746ce88b2018a8ce493700ce1a73b0
Stas Zaitsev <ststas@yandex.ru>
2025-08-10 20:32:10 +0300

    feat: update cv link

--- src/components/Introduction/constants.ts
+++ src/components/Introduction/constants.ts
@@ -1,5 +1,5 @@
 export const CV_BUTTON_LINK =
-  "https://drive.google.com/file/d/1iyTYy745RUs_RnV6AhMNtOS4TsThaf-P/view?usp=drive_link";
+  "https://drive.google.com/file/d/1EAX2YQFxZd-Kk8dlz6xDrR_8loch96vv/view?usp=sharing";
 
 export const PORTFOLIO_BUTTON_LINK =
   "https://drive.google.com/file/d/1ksA2cEhvs6gCyGFiHhC_ILet81ORLLBU/view?usp=drive_link";
